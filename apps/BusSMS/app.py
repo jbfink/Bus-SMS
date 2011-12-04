06aee09e9720811a341207fd7624e6ff26cd5980
Justin Lee <justin@karmatize.com>
2011-12-04 17:21:49 -0500

    Modified app.py to output model data after request;
    Signed-off-by: Justin Lee <justin@karmatize.com>

--- apps/BusSMS/app.py
+++ apps/BusSMS/app.py
@@ -1,6 +1,7 @@
 import re
+from datetime import time
 from rapidsms.apps.base import AppBase
-from BusSMS.models import StopTime, Trip, Route
+from BusSMS.models import StopTime, Trip, Route, Stop
 
 class App(AppBase):
 
@@ -21,8 +22,9 @@ class App(AppBase):
       msg = message.raw_text.strip().upper();
       
       #parse sms into components.  If nothing is matched than the SMS is invalid
-      match = re.match("^(?:" + userid + ")[ -_]*(?P<stop>\d{4})(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0)
-          
+      #match = re.match("^(?:" + userid + ")[ -_]*(?P<stop>\d{4})(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0) #Regex for 4 digits, use this with actual HSR data
+      match = re.match("^(?:" + userid + ")[ \-_]*(?P<stop>[^ ]+)(?:[ \-_]+(?P<bus>[0-9A-Z]{1,4}))?$",msg,0)        
+        
       if(match == None):
           message.respond('Invalid request')
           return 
@@ -30,10 +32,20 @@ class App(AppBase):
       stop = match.group('stop')
       bus = match.group('bus')
       
-      resp = 'stop:' + stop
+      #get the data relating to the stop# that the user has inputted
+      stoptimes = StopTime.objects.filter(stop__stop_id__contains=stop)
+                  
+      resp = ''
+      for item in stoptimes:
+		  resp += str(item)+ '\n'
+      	  		  
+      #resp = 'stop:' + stop
       if(bus):
           resp += ' bus:' + bus
  
       #TODO: implement lookup of bus schedule
-
+      
+     #right now, it's outputting all data for a stopid. It should get the first 3 times for a given stop #
       message.respond(resp)
+      
+     
