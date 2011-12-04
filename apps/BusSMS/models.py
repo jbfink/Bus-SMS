16b48e162693993217f57c4d9efc801a018fe925
Jason <jason.oreilly@gmail.com>
2011-12-04 10:25:22 -0800

    Added new data set

--- apps/BusSMS/models.py
+++ apps/BusSMS/models.py
@@ -30,13 +30,15 @@ class Route(models.Model):
 
    route_long_name = models.CharField(max_length=40)
    route_short_name = models.CharField(max_length=4, unique=True)
+   route_id = models.CharField(max_length=16, unique=True)
 
 class Trip(models.Model):
    
    def __unicode__(self):
       return str(self.pk)
 
-   route = models.ForeignKey(Route)
+   route = models.ForeignKey(Route, to_field='route_id')
+   trip_id = models.CharField(max_length=16, unique=True)
 
 class Stop(models.Model):
 
@@ -51,6 +53,6 @@ class StopTime(models.Model):
       return str(self.pk) + ' ' + str(self.arrival_time)
 
    arrival_time = models.TimeField() 
-   trip = models.ForeignKey(Trip)
+   trip = models.ForeignKey(Trip, to_field='trip_id')
    stop = models.ForeignKey(Stop, to_field='stop_id')
    
